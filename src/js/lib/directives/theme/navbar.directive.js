299e4c2d45870619ce209c660dd87f1636a2e9b7
Marx JMoura <marx.jmoura@gmail.com>
2017-03-24 19:48:13 -0300

    Portfolio navbar

---
+++ src/js/lib/directives/theme/navbar.directive.js
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Dyoub Applications. All rights reserved.
+// Licensed under MIT (https://github.com/dyoub/app/blob/master/LICENSE).
+
+(function () {
+
+    function NavbarDirective() {
+        return {
+            restrict: 'EC',
+            link: function (scope, $element, attrs) {
+                if ($element.hasClass('fixed')) {
+                    $window = angular.element(window);
+
+                    $window.on('scroll', function () {
+                        if ($window.scrollTop()) {
+                            $element.addClass('scrolled');
+                        } else {
+                            $element.removeClass('scrolled');
+                        }
+                    });
+                }
+            }
+        };
+    }
+
+    angular.module('dyoubTheme').directive('navbar', [
+        NavbarDirective
+    ]);
+
+})();
