9096b29650aa0032230870d39556540299f605e5
Marx JMoura <marx.jmoura@gmail.com>
2017-05-24 10:29:53 -0300

    Add validations for date

---
+++ src/js/lib/directives/validations/date-same-or-after.directive.js
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Dyoub Applications. All rights reserved.
+// Licensed under MIT (https://github.com/dyoub/app/blob/master/LICENSE).
+
+(function () {
+
+    function ValidDateAfterDirective($parse) {
+        function link(scope, $element, attrs, ngModelCtrl) {
+            var dateGetter = $parse(attrs.validDateSameOrAfter);
+
+            function validate(date, anotherDate) {
+                var isValid = ngModelCtrl.$isEmpty(date) ||
+                              ngModelCtrl.$isEmpty(anotherDate) ||
+                              moment(date).isSameOrAfter(anotherDate);
+
+                ngModelCtrl.$setValidity('dateSameOrAfter', isValid);
+            }
+
+            scope.$watch(attrs.ngModel, function (date) {
+                validate(date, dateGetter(scope));
+            });
+
+            scope.$watch(function () {
+                return dateGetter(scope);
+            }, function (anotherDate) {
+                validate(ngModelCtrl.$modelValue, anotherDate);
+            });
+        }
+
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: link
+        };
+    }
+
+    angular.module('dyoub.theme').directive('validDateSameOrAfter', [
+        '$parse',
+        ValidDateAfterDirective
+    ]);
+
+})();
---
+++ src/js/lib/directives/validations/date-same-or-before.directive.js
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Dyoub Applications. All rights reserved.
+// Licensed under MIT (https://github.com/dyoub/app/blob/master/LICENSE).
+
+(function () {
+
+    function ValidDateBeforeDirective($locale, $parse) {
+        function link(scope, $element, attrs, ngModelCtrl) {
+            var dateGetter = $parse(attrs.validDateSameOrBefore);
+
+            function validate(date, anotherDate) {
+                var isValid = ngModelCtrl.$isEmpty(date) ||
+                              ngModelCtrl.$isEmpty(anotherDate) ||
+                              moment(date).isSameOrBefore(anotherDate);
+
+                ngModelCtrl.$setValidity('dateSameOrBefore', isValid);
+            }
+
+            scope.$watch(attrs.ngModel, function (date) {
+                validate(date, dateGetter(scope));
+            });
+
+            scope.$watch(function () {
+                return dateGetter(scope);
+            }, function (anotherDate) {
+                validate(ngModelCtrl.$modelValue, anotherDate);
+            });
+        }
+
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: link
+        };
+    }
+
+    angular.module('dyoub.theme').directive('validDateSameOrBefore', [
+        '$locale',
+        '$parse',
+        ValidDateBeforeDirective
+    ]);
+
+})();
